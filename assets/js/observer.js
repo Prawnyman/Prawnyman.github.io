d8a4a0cafe5fdd8c27278711ae4b68d2ac7b88e2
Shawn Lee <prawnyman@gmail.com>
2021-12-21 03:20:45 +0800

    New Alignment

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -3,34 +3,11 @@ const sectionOne = document.querySelector(".home-intro");
 
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
-/*
-const sectionOneOptions = {
-  rootMargin: "-200px 0px 0px 0px"
-};
-
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("nav-scrolled");
-    } else {
-      header.classList.remove("nav-scrolled");
-    }
-  });
-},
-sectionOneOptions);
-
-sectionOneObserver.observe(sectionOne);
-*/
-
-
 
 
 const appearOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
+  rootMargin: "0px 0px -150px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver(function(
